1e7e62e3150c42b030677a003e1f276897c4e415
Teetwothefirst <dexterdavid835@gmail.com>
2023-07-31 07:04:28 +0100

    Changes for the background

--- CloneChat/connect.js
+++ CloneChat/connect.js
@@ -1,1 +1,9 @@
-alert("Welcome");
+
+
+
+function outerChat(){
+    console.log("welcome to the newton")
+    //So for the outer chat section the image would be clickable and the headers as well.
+    //So when you click on the image section it would come out to take up the full size of it's container
+    
+}
