a806eb2813ee387aa71df1405e3c0fcbc4c05e4f
Teetwothefirst <dexterdavid835@gmail.com>
2023-08-01 09:58:13 +0100

    Changes to the UI, Add functionalities next

--- CloneChat/connect.js
+++ CloneChat/connect.js
@@ -2,8 +2,22 @@
 
 
 function outerChat(){
-    console.log("welcome to the newton")
+    console.log("welcome to the newton");
     //So for the outer chat section the image would be clickable and the headers as well.
     //So when you click on the image section it would come out to take up the full size of it's container
+    let outerchatcontainer = document.getElementById("Outer-chat-container");
+    let outerLinkChat = document.getElementById("outer-link-child");
+    let outerLink = document.getElementById("outer-link");
+    let innerChatContainer = document.getElementById("inner-chat-container")
+
+    if (outerLink.click){
+        outerchatcontainer.style = 'display: none';
+        innerChatContainer.style = 'display: block';
+    
+    }else{
+        //let it remain as it should remain as it is
+        
+    }
+
     
 }
